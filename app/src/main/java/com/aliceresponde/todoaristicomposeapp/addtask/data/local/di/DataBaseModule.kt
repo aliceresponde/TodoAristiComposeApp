f79803a78a57c0b5671549d12607ad454b697a1a
aliceLit <Alicia.Beltran@litmos.com>
2023-11-22 11:07:28 -0500

    create Di folder for local CreateDatabaseModule Provide DB, and Daos

---
+++ app/src/main/java/com/aliceresponde/todoaristicomposeapp/addtask/data/local/di/DataBaseModule.kt
@@ -0,0 +1,29 @@
+package com.aliceresponde.todoaristicomposeapp.addtask.data.local.di
+
+import android.content.Context
+import androidx.room.Room
+import com.aliceresponde.todoaristicomposeapp.addtask.data.local.TodoDataBase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataBaseModule {
+    @Provides
+    @Singleton
+    fun provideDataBase(@ApplicationContext context: Context): TodoDataBase {
+        return Room.databaseBuilder(
+            context = context,
+            klass = TodoDataBase::class.java,
+            name = "TASK_DATABASE"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTaskDao(dataBase: TodoDataBase) = dataBase.taskDao()
+}
